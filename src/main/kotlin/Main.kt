5fd19406195ab375d0541eedf886dc960cc50f06
Mikhail Popkov <mikhailpopkovcompany@gmail.com>
2026-01-04 21:44:29 +0300

    Add test data to main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package com.saymk.roadmap
 
+import com.saymk.roadmap.model.Obstacle
+import com.saymk.roadmap.model.Resource
+import com.saymk.roadmap.model.Wolf
+import com.saymk.roadmap.model.common.Coordinates
+
 fun main() {
-    println("im gay!")
+    println(Wolf(Coordinates(1, 9)))
+    println(Obstacle.Rock(Coordinates(1, 9)))
+    println(Obstacle.Tree(Coordinates(1, 9)))
+    println(Resource.Grass(Coordinates(1, 9)))
 }
